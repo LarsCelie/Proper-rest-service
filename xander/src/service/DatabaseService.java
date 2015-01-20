4137a422609cda69c326a19ca447a72d0fa89d6a
Lars <LarsCelie@gmail.com>
2015-01-20 14:09:00 +0100

    broken, somehow

--- xander/src/service/DatabaseService.java
+++ xander/src/service/DatabaseService.java
@@ -1,13 +1,14 @@
 package service;
-
-import model.Database;
-import model.DatabaseImpl;
-
-public class DatabaseService {
-
-	public Database getDatabaseByName(String databaseName) {
-		// TODO Auto-generated method stub
-		return new DatabaseImpl();
-	}
-
-}
+//package service;
+//
+//import model.Database;
+//import model.DatabaseImpl;
+//
+//public class DatabaseService {
+//
+//	public Database getDatabaseByName(String databaseName) {
+//		// TODO Auto-generated method stub
+//		return new DatabaseImpl();
+//	}
+//
+//}
